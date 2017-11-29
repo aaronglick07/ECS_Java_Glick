667333550bf508123ea7d437adca5306e5fe95ba
AaronGlick07 <GlickA0192@sduhsd.lan>
2017-11-29 11:56:40 -0800

    labs
    doing the assaigned labs

---
+++ ECSJavaProject/src/GoodFirstProgram.java
@@ -0,0 +1,19 @@
+
+public class GoodFirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		//This is a comment
+		//under this comment lines are being printed to the console
+		System.out.println("Hello World!");
+		System.out.println("Hello Again");
+		System.out.println("I lkie typing this.");
+		System.out.println("This is fun.");
+		System.out.println("Yay!  Printing.");
+		System.out.println("I'd much rather you 'not'.");
+		System.out.println("I \"said\" do not touch this.");
+		System.out.println("that was a lot of typing!");
+		
+	}
+
+}
